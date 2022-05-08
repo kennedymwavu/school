47320a0b4e923db8180bdf272231e0d17ae43351
evansochieng <evansochieng121@gmail.com>
2022-05-08 13:55:01 +0300

    fitting distributions

--- project.R
+++ project.R
@@ -2,16 +2,17 @@ library(readxl)
 library(tidyverse)
 library(moments)
 library(ggpubr)
+library(fitdistrplus)
 
 industry <- readxl::read_xlsx(
   path = "data/insurance_industry_stats_2016-2020.xlsx", 
   skip = 1
-) |> 
+) %>% 
   dplyr::select(
     `Class Name`, `2016`:`2020`
   )
 
-industry_long <- industry |> 
+industry_long <- industry %>% 
   tidyr::pivot_longer(
     cols = !`Class Name`, 
     names_to = "Year", 
@@ -25,8 +26,8 @@ se <- function(x) {
 }
 
 # descriptive stats:
-desc_stats <- industry_long |> 
-  group_by(`Class Name`) |> 
+desc_stats <- industry_long %>% 
+  group_by(`Class Name`) %>%
   summarise(
     `No. Of Observations` = n(), 
     Mean = mean(Amount), 
@@ -38,13 +39,13 @@ desc_stats <- industry_long |>
     Minimum = min(Amount), 
     Maximum = max(Amount), 
     Sum = sum(Amount)
-  ) |> 
-  t() |> 
+  ) %>% 
+  t() %>% 
   as.data.frame()
 
 # set column names:
-colnames(desc_stats) <- desc_stats[1, 1:2] |> 
-  gsub(pattern = "_", replacement = " ") |> 
+colnames(desc_stats) <- desc_stats[1, 1:2] %>% 
+  gsub(pattern = "_", replacement = " ") %>% 
   stringr::str_to_title()
 
 # remove first row:
@@ -57,12 +58,12 @@ desc_stats["Stat"] <- rownames(desc_stats)
 rownames(desc_stats) <- NULL
 
 # make stat the first column:
-desc_stats <- desc_stats |> 
+desc_stats <- desc_stats %>% 
   dplyr::relocate(Stat)
 
 # ----hist----
-mc_hist <- industry_long |> 
-  dplyr::filter(`Class Name` == "motor_commercial") |> 
+mc_hist <- industry_long %>%
+  dplyr::filter(`Class Name` == "motor_commercial") %>% 
   ggplot(
     aes(x = Amount)
   ) + 
@@ -80,8 +81,8 @@ mc_hist <- industry_long |>
   ) + 
   theme_classic()
 
-mp_hist <- industry_long |> 
-  dplyr::filter(`Class Name` == "motor_private") |> 
+mp_hist <- industry_long %>%
+  dplyr::filter(`Class Name` == "motor_private") %>% 
   ggplot(
     aes(x = Amount)
   ) + 
@@ -106,16 +107,16 @@ mp_hist <- industry_long |>
 # This implies the need to use continuous distributions that 
 # are +vely skewed to fit the data
 
-mc_qqplot <- industry_long |> 
-  dplyr::filter(`Class Name` == "motor_commercial") |> 
+mc_qqplot <- industry_long %>%
+  dplyr::filter(`Class Name` == "motor_commercial") %>% 
   ggqqplot(
     x = "Amount", 
     title = "Motor Commercial", 
     size = 0.8
   )
 
-mp_qqplot <- industry_long |> 
-  dplyr::filter(`Class Name` == "motor_private") |> 
+mp_qqplot <- industry_long %>%
+  dplyr::filter(`Class Name` == "motor_private") %>% 
   ggqqplot(
     x = "Amount", 
     title = "Motor Private", 
@@ -130,19 +131,64 @@ industry_long_trans <- industry_long
 industry_long_trans$Amount <- industry_long_trans$Amount ^ (1 / 3)
 
 # qqplots of transformed data:
-mc_trans_qqplot <- industry_long_trans |> 
-  dplyr::filter(`Class Name` == "motor_commercial") |> 
+mc_trans_qqplot <- industry_long_trans %>% 
+  dplyr::filter(`Class Name` == "motor_commercial") %>% 
   ggqqplot(
     x = "Amount", 
     title = "Motor Commercial", 
     size = 0.8
   )
 
-mp_trans_qqplot <- industry_long_trans |> 
-  dplyr::filter(`Class Name` == "motor_private") |> 
+mp_trans_qqplot <- industry_long_trans %>% 
+  dplyr::filter(`Class Name` == "motor_private") %>% 
   ggqqplot(
     x = "Amount", 
     title = "Motor Private", 
     size = 0.8
   )
 
+# fitting distributions to the data
+
+#Extract positive values for fitting models, x > 0
+positive_data <- industry_long_trans$Amount[industry_long_trans$Amount > 0] %>% 
+  na.omit()
+positive_data <- c(positive_data) #make it a vector
+positive_data
+
+# Fit models
+# (i) Exponential model, x > 0
+exp_model <- fitdist(positive_data, "exp")
+exp_model$estimate       #parameter estimate
+exp_model$sd    #standard error of parameter
+exp_model$loglik    #log likelihood estimate
+gofstat(exp_model) #extract K-S, A-D, AIC, BIC of the model
+
+
+# (ii) Gamma model, x > 0
+gamma_model <- fitdist(positive_data, "gamma")
+gamma_model$estimate       #parameters estimates
+gamma_model$sd    #standard error of estimates
+gamma_model$loglik    #log likelihood estimate
+gofstat(gamma_model) #extract K-S, A-D, AIC, BIC of the model
+
+# (iii) Lognormal model, x > 0
+lognormal_model <- fitdist(positive_data, "lnorm")
+lognormal_model$estimate       #parameters estimates
+lognormal_model$sd    #standard error of estimates
+lognormal_model$loglik    #log likelihood estimate
+gofstat(lognormal_model) #extract K-S, A-D, AIC, BIC of the model
+
+# (iv) Weibull model, x > 0
+weibull_model <- fitdist(positive_data, "weibull")
+weibull_model$estimate       #parameters estimates
+weibull_model$sd    #standard error of estimates
+weibull_model$loglik    #log likelihood estimate
+gofstat(weibull_model) #extract K-S, A-D, AIC, BIC of the model
+
+# (v) Pareto model, x > 0
+par_data <- c(scale(positive_data))
+pareto_model <- fitdist(par_data, "pareto", lower = c(0, 0), start = list(location = 1, shape = 1))
+pareto_model$estimate       #parameters estimates
+pareto_model$sd    #standard error of estimates
+pareto_model$loglik    #log likelihood estimate
+gofstat(pareto_model) #extract K-S, A-D, AIC, BIC of the model
